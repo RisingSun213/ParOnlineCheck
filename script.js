8980be2c11d9a558f1ae5567af3ab675b92af353
RisingSun213 <112540900+RisingSun213@users.noreply.github.com>
2025-04-06 12:51:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -130,10 +130,10 @@ document.getElementById('queryBtn').addEventListener('click', async () => {
                 resultDisplay.innerHTML = "您已参加过本次实验，谢谢！";
                 break;
             case "1":
-                resultDisplay.innerHTML = `您尚未参加本次实验，请点击以下链接进行参与：<a href="https://www.naodao.com/project/947897812933287938" target="_blank">点击这里参加实验</a>`;
+                resultDisplay.innerHTML = `您尚未参加本次实验，请点击以下链接进行参与：<a href="https://www.naodao.com/project/961190758210342914" target="_blank">点击这里参加实验</a>`;
                 break;
             case "2":
-                resultDisplay.innerHTML = `您尚未参加本次实验，请点击以下链接进行参与：<a href="https://www.naodao.com/project/947897718368636930" target="_blank">点击这里参加实验</a>`;
+                resultDisplay.innerHTML = `您尚未参加本次实验，请点击以下链接进行参与：<a href="https://www.naodao.com/project/960864791961014274" target="_blank">点击这里参加实验</a>`;
                 break;
             case "3":
                 resultDisplay.innerHTML = "抱歉，当前实验轮次已招满，您可以参与后续的实验：";
