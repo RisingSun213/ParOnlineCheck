883567dc3e95103d4b93a686cb6307dc1bfdd14e
RisingSun213 <112540900+RisingSun213@users.noreply.github.com>
2025-04-21 16:57:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -134,6 +134,7 @@ document.getElementById('queryBtn').addEventListener('click', async () => {
                 break;
             case "2":
                 resultDisplay.innerHTML = `您尚未参加本次实验，请点击以下链接进行参与：<a href="https://www.naodao.com/project/966729214277718018" target="_blank">点击这里参加实验</a>`;
+                break;
             case "3":
                 resultDisplay.innerHTML = "抱歉，当前实验轮次已招满，您可以参与后续的实验：";
                 break;
