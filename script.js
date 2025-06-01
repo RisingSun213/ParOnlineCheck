2c860b4561410138ce716352faae9541491694ef
RisingSun213 <112540900+RisingSun213@users.noreply.github.com>
2025-06-01 20:55:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -134,7 +134,7 @@ document.getElementById('queryBtn').addEventListener('click', async () => {
                 resultDisplay.innerHTML = "您已参加过本次实验，谢谢！";
                 break;
             case "1":
-                resultDisplay.innerHTML = `您尚未参加本次实验，请点击以下链接进行参与：<a href="https://www.naodao.com/project/980132511093100546" target="_blank">点击这里参加实验</a>`;
+                resultDisplay.innerHTML = `您尚未参加本次实验，请点击以下链接进行参与：<a href="https://www.naodao.com/project/980130893350047746" target="_blank">点击这里参加实验</a>`;
                 break;
             case "2":
                 resultDisplay.innerHTML = `您尚未参加本次实验，请点击以下链接进行参与：<a href="https://www.naodao.com/project/980130893350047746" target="_blank">点击这里参加实验</a>`;
